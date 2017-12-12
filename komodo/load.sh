4a33feef372d4ab0efca40f69db4b5cfd4994df3
Ramesh Reddy <rareddy@jboss.org>
2017-12-12 10:09:34 -0600

    Load script

---
+++ komodo/load.sh
@@ -0,0 +1,6 @@
+!#/bin/sh
+
+oc delete is komodo -n openshift
+oc delete template data-access-service -n openshift
+oc create -f komodo-image-streams.json -n openshift
+oc create -f data-access-service.json -n openshift
