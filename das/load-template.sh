d5b89c6e2d2871f79d0a61d7b339a31c672286b3
Ramesh Reddy <rareddy@jboss.org>
2018-03-08 07:37:22 -0600

    TEIIDTOOLS-351: Adding beetle from prebuilt docker image rather than using the s2i build

--- das/load-template.sh
+++ das/load-template.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 oc delete is komodo -n openshift
+oc delete is beetle -n openshift
 oc delete template data-access-service -n openshift
+
 oc create -f komodo-image-streams.json -n openshift
+oc create -f beetle-image-streams.json -n openshift
 oc create -f data-access-service.json -n openshift
